bf9dfb7274fa6e5f065a365ec4acb199e06747a7
IyadAbed <iyadrabed@gmail.com>
2023-05-22 12:54:38 +0300

    create new project

---
+++ front/src/App.js
@@ -0,0 +1,29 @@
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import AboutUs from "./Pages/AboutUs";
+import ContactUs from "./Pages/ContactUs";
+import SignUp from "./Pages/SignUp";
+import List from "./Pages/List";
+import LogIn from "./Pages/LogIn";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <BrowserRouter>
+    <NavBar />
+      <Routes>
+        <Route element={<Home/>} path="/"/>
+        <Route element={<AboutUs/>} path="/AboutUs"/>
+        <Route element={<ContactUs/>} path="/ContactUs"/>
+        <Route element={<SignUp/>} path="/SignUp"/>
+        <Route element={<LogIn/>} path="/LogIn"/>
+        <Route element={<List/>} path="/List"/>
+      </Routes>
+    <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
